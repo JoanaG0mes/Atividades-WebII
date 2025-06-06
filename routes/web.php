ae8c4adf1949cf9aa5b89cb7930add55bb79dda5
JoanaG0mes <joana.gomes2377@gmail.com>
2025-06-06 00:15:14 -0300

    Atividade 5

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\PublisherController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('categories', CategoryController::class);
+Route::resource('authors', AuthorController::class);
+Route::resource('publishers', PublisherController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
